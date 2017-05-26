c9949680e02570bf801e6238ab329db0b3b7acba
SwordArtist <yusuke.dev1@gmail.com>
2017-05-26 11:05:06 -0600

    Had issue for layout status. When I expand view, some other position of view were expanded.
    Added a list to hold a LinearLayout visibility statuses. It is used in onBindViewHolder to set the layout visibility.

--- app/src/main/java/com/swordartist/recyclerviewapp/MyAdapter.java
+++ app/src/main/java/com/swordartist/recyclerviewapp/MyAdapter.java
@@ -1,7 +1,6 @@
 package com.swordartist.recyclerviewapp;
 
 import android.content.Context;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,10 +9,12 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implements View.OnClickListener {
     private String[] mDataset;
     private Context mContext;
+    private ArrayList<Boolean> detailLayoutExpandStates = new ArrayList<>();
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -31,6 +32,11 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implem
     public MyAdapter(Context context, String[] myDataset) {
         mContext = context;
         mDataset = myDataset;
+
+        // initialize layout state list
+        for (int i = 0; i < mDataset.length; i++) {
+            detailLayoutExpandStates.add(false);
+        }
     }
 
     // Create new views (invoked by the layout manager)
@@ -39,9 +45,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implem
                                                    int viewType) {
         // create a new view
         CardView v = (CardView) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.billing_overview_card, parent, false);
+                .inflate(R.layout.overview_card, parent, false);
         // set the view's size, margins, paddings and layout parameters
-        ConstraintLayout constraintLayout = (ConstraintLayout) v.findViewById(R.id.constraint_layout);
 
         ViewHolder vh = new ViewHolder(v);
         return vh;
@@ -53,8 +58,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implem
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         TextView textView = (TextView) holder.mCardView.findViewById(R.id.textView11);
-
         textView.setText(mDataset[position]);
+
+        // set visibility for detail layout based on the saved status 
+        LinearLayout detailLayout = (LinearLayout) holder.mCardView.findViewById(R.id.detail_linearlayout);
+        if (detailLayoutExpandStates.get(position)) {
+            detailLayout.setVisibility(View.VISIBLE);
+        } else {
+            detailLayout.setVisibility(View.GONE);
+        }
 
         MyButton viewDetailButton = (MyButton) ((View) textView.getParent().getParent()).findViewById(R.id.view_detail_btn);
         viewDetailButton.setOnClickListenerCustom(this, position);
@@ -68,11 +80,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implem
 
     @Override
     public void onClick(View v) {
-        LinearLayout billingDetailLayout = (LinearLayout) ((View) v.getParent().getParent()).findViewById(R.id.billing_detail_layout);
-        if (billingDetailLayout.getVisibility() == View.GONE) {
-            billingDetailLayout.setVisibility(View.VISIBLE);
+        // Change detail layout visibility, also save the status.
+        LinearLayout detailLayout = (LinearLayout) ((View) v.getParent().getParent()).findViewById(R.id.detail_linearlayout);
+        if (detailLayout.getVisibility() == View.GONE) {
+            detailLayoutExpandStates.set(((MyButton) v).position, Boolean.TRUE);
+            detailLayout.setVisibility(View.VISIBLE);
         } else {
-            billingDetailLayout.setVisibility(View.GONE);
+            detailLayoutExpandStates.set(((MyButton) v).position, Boolean.FALSE);
+            detailLayout.setVisibility(View.GONE);
         }
     }
 }
